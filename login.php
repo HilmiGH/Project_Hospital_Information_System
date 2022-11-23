f7d3873a342d03107a10fc9c017072bba09318af
Hilmi G <99980714+HilmiGH@users.noreply.github.com>
2022-11-23 17:45:10 +0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="css/style.css">
+    <script src="js/script.js"></script>
+</head>
+<body>
+    <div class="login_background">
+        <div class="login_middle_container">
+            <button class="login_close_button_base">
+                <img class="login_close_button" src="images/close_button_test.png" alt="close button">
+            </button>
+            <div class="login_title">
+                Login/Register
+            </div>
+            <div class="login_sub_title_1">
+                Selamat datang!
+            </div>
+            <div class="login_sub_title_2">
+                Silahkan masukkan nomor telpon Anda
+            </div>
+            <div class="login_form_title">
+                Nomor Telepon
+            </div>
+            <div class="login_input_and_button">
+                <form action="" class="login_phone_input_base">
+                    <input type="text" class="login_phone_input" id="login_phone_input">
+                </form>
+                <a href="login_otp.php">
+                    <button class="login_button" type="button" onclick="alert('Its working')">
+                        LANJUTKAN
+                    </button>
+                </a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ login_otp.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login OTP</title>
+    <link rel="stylesheet" href="css/style.css">
+    <script src="js/script.js"></script>
+</head>
+<body>
+<div class="login_otp_background">
+        <div class="login_otp_middle_container">
+            <div class="login_otp_title">
+                Kode OTP
+            </div>
+            <div class="login_otp_sub_title_1">
+                Masukkan kode OTP 4 digit
+            </div>
+            <div class="login_otp_sub_title_2">
+                yang telah terkirim ke nomor Anda
+            </div>
+            <div class="login_otp_input_and_button">
+                <form action="" class="login_otp_phone_input_base">
+                    <input type="text" class="login_otp_phone_input" id="login_phone_input">
+                    <input type="text" class="login_otp_phone_input" id="login_phone_input">
+                    <input type="text" class="login_otp_phone_input" id="login_phone_input">
+                    <input type="text" class="login_otp_phone_input" id="login_phone_input">
+                </form>
+                <a href=""></a>
+                <button class="login_otp_button" type="button" onclick="alert('Its working')">
+                    LANJUTKAN
+                </button>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
