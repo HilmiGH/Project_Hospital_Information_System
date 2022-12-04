66355c7de63ec70282c70650e5c06828f5a5c873
Hilmi G <99980714+HilmiGH@users.noreply.github.com>
2022-12-04 11:09:18 +0700

    Update index_dokter.php

--- index_dokter.php
+++ index_dokter.php
@@ -4,22 +4,22 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard_Dokter</title>
+    <title>Dashboard Dokter</title>
     <link rel="stylesheet" href="css/style.css">
     <script src="js/script.js"></script>
 </head>
 <body>
     <div class="header">
-        <a href="/index_dokter.php">
-            <img class="logo" src="images/logo_test.png" alt="logo">
-        </a>
-        <a href="/index_dokter.php">
-            <img class="logout_button" src="images/Logout_button_test.png" alt="log out button">
+        <span class="logo">
+            Your logo
+        </span>
+        <a href="login.php">
+            <img class="logout_button" src="images/dashboard_logout_button.png" alt="log out button">
         </a>
     </div>
     <div class="top_banner">
         <div>
-            <img class="profile_logo"src="images/profile_logo_test.png" alt="profile logo">
+            <img class="profile_logo"src="images/dashboard_account_logo.png" alt="profile logo">
         </div>
         <span class="greeting">
             <strong>Selamat Datang!</strong>
@@ -28,238 +28,130 @@
         </span>
     </div>
     <div class="main">
-        <div>
-            <div class="catagory">
-                Fitur Utama
-            </div>
-        </div>
         <div class="main_dashboard">
             <div class="button_base">
                 <div class="dashboard_button">
-                    <a href="">
+                    <a href="konsultasi.php" class="button_clickable">
                         <div class="button_content">
-                            <img class= "button_logo" src="images/medicine_test.png" alt="Medicine logo">
+                            <img class= "button_logo" src="images/dashboard_konsultasi_logo.png" alt="Logo Konsultasi">
+                        </div>
+                        <div class="button_title">
+                                Konsultasi
                         </div>
                     </a>
                 </div>
             </div>
             <div class="button_base">
                 <div class="dashboard_button">
-                    <a href="">
+                    <a href="visite.php" class="button_clickable">
                         <div class="button_content">
-                            <img class= "button_logo" src="images/medicine_test.png" alt="Medicine logo">
+                            <img class= "button_logo" src="images/dashboard_visite_logo.png" alt="Logo Visite">
+                        </div>
+                        <div class="button_title">
+                            Visite
                         </div>
                     </a>
                 </div>
             </div>
             <div class="button_base">
                 <div class="dashboard_button">
-                    <a href="">
+                    <a href="" class="button_clickable">
                         <div class="button_content">
-                            <img class= "button_logo" src="images/medicine_test.png" alt="Medicine logo">
+                            <img class= "button_logo" src="images/dashboard_rujukan_logo.png" alt="Rujukan logo">
+                        </div>
+                        <div class="button_title">
+                            Rujukan
                         </div>
                     </a>
                 </div>
             </div>
             <div class="button_base">
                 <div class="dashboard_button">
-                    <a href="">
+                    <a href="" class="button_clickable">
                         <div class="button_content">
-                            <img class= "button_logo" src="images/medicine_test.png" alt="Medicine logo">
+                            <img class= "button_logo" src="images/dashboard_buat_surat_logo.png" alt="Logo Buat Surat">
+                        </div>
+                        <div class="button_title">
+                        Buat Surat
                         </div>
                     </a>
                 </div>
             </div>
             <div class="button_base">
                 <div class="dashboard_button">
-                    <a href="">
+                    <a href="" class="button_clickable">
                         <div class="button_content">
-                            <img class= "button_logo" src="images/medicine_test.png" alt="Medicine logo">
+                            <img class= "button_logo" src="images/dashboard_laporan_logo.png" alt="Logo Laporan ">
+                        </div>
+                        <div class="button_title">
+                            Laporan
                         </div>
                     </a>
                 </div>
             </div>
             <div class="button_base">
                 <div class="dashboard_button">
-                    <a href="">
+                    <a href="" class="button_clickable">
                         <div class="button_content">
-                            <img class= "button_logo" src="images/medicine_test.png" alt="Medicine logo">
+                            <img class= "button_logo" src="images/dashboard_profile_logo.png" alt="Logo Profile">
+                        </div>
+                        <div class="button_title">
+                                Profile
                         </div>
                     </a>
                 </div>
             </div>
             <div class="button_base">
                 <div class="dashboard_button">
-                    <a href="">
+                    <a href="" class="button_clickable">
                         <div class="button_content">
-                            <img class= "button_logo" src="images/medicine_test.png" alt="Medicine logo">
+                            <img class= "button_logo" src="images/dashboard_schedule_logo.png" alt="Logo Schedule">
+                        </div>
+                        <div class="button_title">
+                            Schedule
                         </div>
                     </a>
                 </div>
             </div>
             <div class="button_base">
                 <div class="dashboard_button">
-                    <a href="">
+                    <a href="" class="button_clickable">
                         <div class="button_content">
-                            <img class= "button_logo" src="images/medicine_test.png" alt="Medicine logo">
+                            <img class= "button_logo" src="images/dashboard_history_logo.png" alt="Logo History">
+                        </div>
+                        <div class="button_title">
+                            History
                         </div>
                     </a>
                 </div>
             </div>
             <div class="button_base">
                 <div class="dashboard_button">
-                    <a href="">
+                    <a href="settings.php" class="button_clickable">
                         <div class="button_content">
-                            <img class= "button_logo" src="images/medicine_test.png" alt="Medicine logo">
+                            <img class= "button_logo" src="images/dashboard_settings_logo.png" alt="Logo Settings">
+                        </div>
+                        <div class="button_title">
+                            Settings
                         </div>
                     </a>
                 </div>
             </div>
             <div class="button_base">
                 <div class="dashboard_button">
-                    <a href="">
+                    <a href="" class="button_clickable">
                         <div class="button_content">
-                            <img class= "button_logo" src="images/medicine_test.png" alt="Medicine logo">
+                            <img class= "button_logo" src="images/dashboard_about_logo.png" alt="Logo About">
                         </div>
-                    </a>
-                </div>
-            </div>
-            <div class="button_base">
-                <div class="dashboard_button">
-                    <a href="">
-                        <div class="button_content">
-                            <img class= "button_logo" src="images/medicine_test.png" alt="Medicine logo">
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <div class="button_base">
-                <div class="dashboard_button">
-                    <a href="">
-                        <div class="button_content">
-                            <img class= "button_logo" src="images/medicine_test.png" alt="Medicine logo">
-                        </div>
-                    </a>
-                </div>
-            </div>
-        </div>
-        <div>
-            <div class="catagory">
-                Fitur Tambahan
-            </div>
-        </div>
-        <div class="main_dashboard">
-            <div class="button_base">
-                <div class="dashboard_button">
-                    <a href="">
-                        <div class="button_content">
-                            <img class= "button_logo" src="images/medicine_test.png" alt="Medicine logo">
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <div class="button_base">
-                <div class="dashboard_button">
-                    <a href="">
-                        <div class="button_content">
-                            <img class= "button_logo" src="images/medicine_test.png" alt="Medicine logo">
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <div class="button_base">
-                <div class="dashboard_button">
-                    <a href="">
-                        <div class="button_content">
-                            <img class= "button_logo" src="images/medicine_test.png" alt="Medicine logo">
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <div class="button_base">
-                <div class="dashboard_button">
-                    <a href="">
-                        <div class="button_content">
-                            <img class= "button_logo" src="images/medicine_test.png" alt="Medicine logo">
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <div class="button_base">
-                <div class="dashboard_button">
-                    <a href="">
-                        <div class="button_content">
-                            <img class= "button_logo" src="images/medicine_test.png" alt="Medicine logo">
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <div class="button_base">
-                <div class="dashboard_button">
-                    <a href="">
-                        <div class="button_content">
-                            <img class= "button_logo" src="images/medicine_test.png" alt="Medicine logo">
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <div class="button_base">
-                <div class="dashboard_button">
-                    <a href="">
-                        <div class="button_content">
-                            <img class= "button_logo" src="images/medicine_test.png" alt="Medicine logo">
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <div class="button_base">
-                <div class="dashboard_button">
-                    <a href="">
-                        <div class="button_content">
-                            <img class= "button_logo" src="images/medicine_test.png" alt="Medicine logo">
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <div class="button_base">
-                <div class="dashboard_button">
-                    <a href="">
-                        <div class="button_content">
-                            <img class= "button_logo" src="images/medicine_test.png" alt="Medicine logo">
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <div class="button_base">
-                <div class="dashboard_button">
-                    <a href="">
-                        <div class="button_content">
-                            <img class= "button_logo" src="images/medicine_test.png" alt="Medicine logo">
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <div class="button_base">
-                <div class="dashboard_button">
-                    <a href="">
-                        <div class="button_content">
-                            <img class= "button_logo" src="images/medicine_test.png" alt="Medicine logo">
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <div class="button_base">
-                <div class="dashboard_button">
-                    <a href="">
-                        <div class="button_content">
-                            <img class= "button_logo" src="images/medicine_test.png" alt="Medicine logo">
+                        <div class="button_title">
+                            About
                         </div>
                     </a>
                 </div>
             </div>
         </div>
         <div class="footer">
-            Copyright Bhayang
+            © 2022 Bhayangkara
         </div>
     </div>
 </body>
