85058165bb53b775461e8732a00fd3b918aa696a
NapCodeKin <100124866+NapCodeKin@users.noreply.github.com>
2022-12-05 18:33:34 +0700

    Add files via upload

---
+++ index/index_nakes.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard Nakes</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style2.css">
+    <script src="js/script.js"></script>
+</head>
+<body>
+    <div class="header">
+        <span class="logo">
+            Your logo
+        </span>
+        <a href="login.php">
+            <img class="logout_button" src="images/dashboard_logout_button.png" alt="log out button">
+        </a>
+    </div>
+    <div class="top_banner top_banner_nakes">
+        <div>
+            <img class="profile_logo"src="images/dashboard_account_logo.png" alt="profile logo">
+        </div>
+        <span class="greeting">
+            <strong>Selamat Datang!</strong>
+            <br />
+            Nama Nakes
+        </span>
+    </div>
+    <div class="main">
+        <div class="main_dashboard">
+            <div class="button_base">
+                <div class="dashboard_button dashboard_button_nakes">
+                    <a href="".php" class="button_clickable">
+                        <div class="button_content">
+                            <img class= "button_logo" src="images/dashboard_data_logo.png" alt="Logo Konsultasi">
+                        </div>
+                        <div class="button_title">
+                            Data
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="button_base">
+                <div class="dashboard_button dashboard_button_nakes">
+                    <a href="" class="button_clickable">
+                        <div class="button_content">
+                            <img class= "button_logo" src="images/dashboard_pasien_logo.png" alt="Logo Visite">
+                        </div>
+                        <div class="button_title">
+                            Pasien
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="button_base">
+                <div class="dashboard_button dashboard_button_nakes">
+                    <a href="" class="button_clickable">
+                        <div class="button_content">
+                            <img class= "button_logo" src="images/dashboard_laporan_logo.png" alt="Logo Laporan ">
+                        </div>
+                        <div class="button_title">
+                            Laporan
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="button_base">
+                <div class="dashboard_button dashboard_button_nakes">
+                    <a href="" class="button_clickable">
+                        <div class="button_content">
+                            <img class= "button_logo" src="images/dashboard_profile_logo.png" alt="Logo Profile">
+                        </div>
+                        <div class="button_title">
+                                Profile
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="button_base">
+                <div class="dashboard_button dashboard_button_nakes">
+                    <a href="" class="button_clickable">
+                        <div class="button_content">
+                            <img class= "button_logo" src="images/dashboard_schedule_logo.png" alt="Logo Schedule">
+                        </div>
+                        <div class="button_title">
+                            Schedule
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="button_base">
+                <div class="dashboard_button dashboard_button_nakes">
+                    <a href="" class="button_clickable">
+                        <div class="button_content">
+                            <img class= "button_logo" src="images/dashboard_history_logo.png" alt="Logo History">
+                        </div>
+                        <div class="button_title">
+                            History
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="button_base">
+                <div class="dashboard_button dashboard_button_nakes">
+                    <a href="settings.php" class="button_clickable">
+                        <div class="button_content">
+                            <img class= "button_logo" src="images/dashboard_settings_logo.png" alt="Logo Settings">
+                        </div>
+                        <div class="button_title">
+                            Settings
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="button_base">
+                <div class="dashboard_button dashboard_button_nakes">
+                    <a href="" class="button_clickable">
+                        <div class="button_content">
+                            <img class= "button_logo" src="images/dashboard_about_logo.png" alt="Logo About">
+                        </div>
+                        <div class="button_title">
+                            About
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="footer">
+            © 2022 Bhayangkara
+        </div>
+    </div>
+</body>
+</html>
