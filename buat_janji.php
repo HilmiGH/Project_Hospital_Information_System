11d735009b98a9ced7a60c7dd172c271d54f0c2c
Hilmi G <99980714+HilmiGH@users.noreply.github.com>
2022-12-14 10:29:13 +0700

    Add files via upload
    Updated the page to include the CSS and JS codes used to display and hid the forms

--- buat_janji.php
+++ buat_janji.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Buat Janji</title>
     <link rel="stylesheet" href="css/style.css">
-    <script src="js/script.js"></script>
+    
     <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
 </head>
 <body>
@@ -31,16 +31,16 @@
         </div>
         <div class="janji_btn_base">
             <div>
-                <button class="janji_poli_btn">Poliklinik</button>
+                <button class="janji_poli_btn" id="poliklinik_btn" onclick="poli_show()">Poliklinik</button>
             </div>
             <div>
-                <button class="janji_labo_btn">Laboratorium</button>
+                <button class="janji_labo_btn" id="laboratorium_btn" onclick="lab_show()">Laboratorium</button>
             </div>
             <div>
-                <button class="janji_fisi_btn">Fisioterapi</button>
+                <button class="janji_fisi_btn" id="fisioterapi_btn" onclick="fisio_show()">Fisioterapi</button>
             </div>
         </div>
-            <div class="janji_content">
+        <div class="janji_content_poliklinik" id="janji_poliklinik">
                 <div class="janji_nama_pasien">
                     Lokasi Rumah Sakit
                     <form  action="" class="janji_jenis_input_base" method="post">
@@ -83,8 +83,75 @@
                     <span>Cari</span>
                 </button>
             </div>
+            <div class="janji_content_laboratorium" id="janji_laboratorium">
+                <div class="janji_nama_pasien">
+                    Lokasi Rumah Sakit
+                    <form  action="" class="janji_jenis_input_base" method="post">
+                        <select class="janji_jenis_surat" name="" id="">
+                            <option value=""></option>
+                        </select>
+                    </form>
+                </div>
+                <div class="janji_nama_pasien">
+                    Tanggal Konsultasi
+                    <form  action="" class="janji_tanggal_input_base" method="post">
+                        <input type="date" class="janji_tanggal_input">
+                    </form>
+                </div>
+                <div class="janji_nama_pasien">
+                    Tujuan
+                    <form  action="" class="janji_tanggal_input_base" method="post">
+                        <input type="text" class="janji_tanggal_input">
+                    </form>
+                </div>
+                <div class="janji_nama_pasien">
+                    Penjaminan
+                    <form  action="" class="janji_jenis_input_base" method="post">
+                        <select class="janji_jenis_surat" name="" id="">
+                            <option value=""></option>
+                        </select>
+                    </form>
+                </div>
+                <button class="janji_cari_button" name="login" type="submit" onclick="">
+                    <span>Cari</span>
+                </button>
+            </div>
+            <div class="janji_content_fisioterapi" id="janji_fisioterapi">
+                <div class="janji_nama_pasien">
+                    Lokasi Rumah Sakit
+                    <form  action="" class="janji_jenis_input_base" method="post">
+                        <select class="janji_jenis_surat" name="" id="">
+                            <option value=""></option>
+                        </select>
+                    </form>
+                </div>
+                <div class="janji_nama_pasien">
+                    Tanggal Terapi
+                    <form  action="" class="janji_tanggal_input_base" method="post">
+                        <input type="date" class="janji_tanggal_input">
+                    </form>
+                </div>
+                <div class="janji_nama_pasien">
+                    Jam
+                    <form  action="" class="janji_tanggal_input_base" method="post">
+                        <input type="time" class="janji_tanggal_input">
+                    </form>
+                </div>
+                <div class="janji_nama_pasien">
+                    Penjaminan
+                    <form  action="" class="janji_jenis_input_base" method="post">
+                        <select class="janji_jenis_surat" name="" id="">
+                            <option value=""></option>
+                        </select>
+                    </form>
+                </div>
+                <button class="janji_cari_button" name="login" type="submit" onclick="">
+                    <span>Cari</span>
+                </button>
+            </div>        
     </div>
     <div class="system_information_footer">
     </div>
+    <script src="js/script.js"></script>
 </body>
 </html>
