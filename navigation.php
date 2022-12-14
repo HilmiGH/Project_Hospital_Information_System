b8051ffcd0799ab520f0bdb613ea1337a843af7d
geprekmumbul <76841943+geprekmumbul@users.noreply.github.com>
2022-12-14 20:11:56 +0700

    Delete navigation.php

--- navigation.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    echo 
-    '<div class="konsultasi_title_navigation">
-        <a href="index_dokter.php" class="konsultasi_back_button_clickable">
-            <img src="images/system_information_back_button.png" alt="Back button" class="konsultasi_back_button">
-        </a>
-    <div class="konsultasi_title">Menu</div>
-    </div>';
-?>
