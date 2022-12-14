9977de48352b2e8dce0eedc8049baf4489a97416
geprekmumbul <76841943+geprekmumbul@users.noreply.github.com>
2022-12-14 20:03:56 +0700

    Delete header1.php

--- header1.php
+++
@@ -1,8 +0,0 @@
-<?php
-    require_once 'session.php';
-    require_once 'head.php';
-    echo '<body>';
-    require_once 'header.php';
-    echo '<div class="konsultasi_content">';
-    require_once 'navigation.php';
-?>
