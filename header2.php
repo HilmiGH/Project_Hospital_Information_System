7d73b3a43f9a86ea4677461d424cc82e62662fb5
geprekmumbul <76841943+geprekmumbul@users.noreply.github.com>
2022-12-14 20:04:07 +0700

    Delete header2.php

--- header2.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    require_once 'session.php';
-    require_once 'head.php';
-    echo '<body>';
-    require_once 'header.php';
-    require_once 'greetings.php';
-    echo '<div class="main">';
-?>
