4feedf3b5c355a72e7de09ebf3def2b1373b6208
Hilmi G <99980714+HilmiGH@users.noreply.github.com>
2022-12-15 09:23:34 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -24,3 +24,31 @@ function fisio_show() {
     janji_laboratorium.style.display='none'
     janji_fisioterapi.style.display='block'
 };
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
