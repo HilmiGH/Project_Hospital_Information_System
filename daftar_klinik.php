a2e33179f3a8b080a2f1badece0092c66bd26b33
NapCodeKin <100124866+NapCodeKin@users.noreply.github.com>
2022-12-15 10:00:16 +0700

    Add files via upload

---
+++ daftar_klinik.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Daftar Klinik</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style3.css">
+    <script src="js/script.js"></script>
+    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+</head>
+<body>
+    <div class="landing_page_header">
+        <div class="landing_page_logo">Your logo</div>
+        <div class="landing_page_nav_button">
+            <a class="landing_page_nav_text" href="">Tentang Kami</a>
+        </div>
+        <div class="landing_page_nav_button">
+            <a class="landing_page_nav_text" href="">Direktori</a>
+        </div>
+        <div class="landing_page_nav_button">
+            <a class="landing_page_nav_text" href="">Buat Janji</a>
+        </div>
+        <div class="landing_page_nav_button">
+            <a class="landing_page_nav_text" href="">Kontak</a>
+        </div>
+        <div class="landing_page_nav_button">
+            <button class="landing_page_login_button">
+                Login
+            </button>
+        </div>
+        <hr>
+    </div>
+    <div class="top_banner_klinik_kami">
+        <div class="klinik_kami_title">
+            Klinik Kami
+        </div>
+    </div>
+    <div class="main">
+        <div class="main_dashboard_4_tiles">
+            <div class="button_base">
+                <div class="dashboard_button_klinik_kami">
+                    <a href="" class="button_clickable">
+                        <div class="button_content">
+                            <img class= "button_logo" src="images/klinik_kami_bayi.png">
+                        </div>
+                        <div class="button_title">
+                            Poliklinik Anak
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="button_base">
+                <div class="dashboard_button_klinik_kami">
+                    <a href="" class="button_clickable">
+                        <div class="button_content">
+                            <img class= "button_logo" src="images/klinik_kami_pregnant.png">
+                        </div>
+                        <div class="button_title">
+                            Poliklinik Kebidanan dan Kandungan
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="button_base">
+                <div class="dashboard_button_klinik_kami">
+                    <a href="" class="button_clickable">
+                        <div class="button_content">
+                            <img class= "button_logo" src="images/klinik_kami_gigi.png">
+                        </div>
+                        <div class="button_title">
+                            Poliklinik Gigi
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="button_base">
+                <div class="dashboard_button_klinik_kami">
+                    <a href="" class="button_clickable">
+                        <div class="button_content">
+                            <img class= "button_logo" src="images/klinik_kami_mata.png">
+                        </div>
+                        <div class="button_title">
+                            Poliklinik Mata
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="button_base">
+                <div class="dashboard_button_klinik_kami">
+                    <a href="" class="button_clickable">
+                        <div class="button_content">
+                            <img class= "button_logo" src="images/klinik_kami_jantung.png">
+                        </div>
+                        <div class="button_title">
+                            Poliklinik Jantung
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="button_base">
+                <div class="dashboard_button_klinik_kami">
+                    <a href="" class="button_clickable">
+                        <div class="button_content">
+                            <img class= "button_logo" src="images/klinik_kami_syaraf.png">
+                        </div>
+                        <div class="button_title">
+                            Poliklinik Syaraf
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="button_base">
+                <div class="dashboard_button_klinik_kami">
+                    <a href="" class="button_clickable">
+                        <div class="button_content">
+                            <img class= "button_logo" src="images/klinik_kami_paru.png">
+                        </div>
+                        <div class="button_title">
+                            Poliklinik Toraks
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="button_base">
+                <div class="dashboard_button_klinik_kami">
+                    <a href="" class="button_clickable">
+                        <div class="button_content">
+                            <img class= "button_logo" src="images/klinik_kami_tulang.png">
+                        </div>
+                        <div class="button_title">
+                            Poliklinik Tulang
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+            <div class="klinik_kami_background_img_base">
+                <img class="klinik_kami_background_img" src="images/klinik_kami_tangan.png">
+            </div>
+        <div class="klinik_kami_footer">
+            © 2022 Bhayangkara
+        </div>
+    </div>
+</body>
+</html>
